27aea47a75c25d64a8dd237b0a9ce5f757631614
Rahul Kundu <121861377+rahulkundu1209@users.noreply.github.com>
2025-11-16 12:14:11 +0530

    change logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logo-r-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logo-r.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/icon.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo-r.png',
   },
 }
 
